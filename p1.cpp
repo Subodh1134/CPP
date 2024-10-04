636de276e880ff057028bdbd8fd6705a5c0e6d3d
Subodh0311 <112371308+Subodh0311@users.noreply.github.com>
2024-10-04 21:41:20 +0530

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!\n";
+    cout<<"Namaste"<<endl;
+    int a =123;
+    cout<<sizeof(a)<<endl;
+    char b='a';
+    cout<<b<<endl;
+    string c="hello";
+    cout<<c<<endl;
+    int d='a';
+    cout<<d<<endl;
+    char e=97;
+    cout<<e<<endl;
+    int f=-5;
+    cout<<f<<endl;
+    float g=2.0;
+    float x=1.0+g;
+    cout<<x<<endl;
+    bool check=('a'==97);
+    cout<<check<<endl;
+    return 0;
+}
