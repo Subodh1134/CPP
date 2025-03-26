7eb0b0e93dc195b883325974552023a5b1a6fbfd
Subodh1134 <subodhrao63246@gmail.com>
2025-03-26 22:19:54 +0530

    Add files via upload

---
+++ hashing/charHashing.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    int n=str.length();
+    int hash[100000]={0};
+    for(int i=0;i<n;i++){
+        hash[str[i]]+=1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        char ch;
+        cin>>ch;
+
+        cout<<ch<<" Appears "<<hash[ch]<<" times"<<endl;
+    }
+}
